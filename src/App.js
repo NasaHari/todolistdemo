134be4df747730e8f71103bd68e20390bc9e21c9
harikrishnan <hariarjav@gmail.com>
2023-07-08 10:51:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Task from "./Task";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleInputKeyPress = (e) => {
+    if (e.key === "Enter") {
+      addTodo();
+    }
+  };
+
+  const addTodo = () => {
+    if (inputValue.trim() !== "") {
+      setTodos([...todos, { text: inputValue, completed: false }]);
+      setInputValue("");
+    }
+  };
+
+  const toggleTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos.splice(index, 1);
+    setTodos(updatedTodos);
+  };
+
+  const completedTodos = todos.filter((todo) => todo.completed);
+  const incompleteTodos = todos.filter((todo) => !todo.completed);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>To-Do List</h1>
+      <div className="input-container">
+        <input
+          className="input"
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          onKeyPress={handleInputKeyPress}
+          placeholder="Add a to-do item..."
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <ul>
+        {incompleteTodos.map((todo, index) => (
+          <Task
+            key={index}
+            index={index}
+            todo={todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ul>
+      {completedTodos.length > 0 && (
+        <>
+          <h2>Completed Items</h2>
+          <ul>
+            {completedTodos.map((todo, index) => (
+              <Task
+                key={index}
+                index={index}
+                todo={todo}
+                toggleTodo={toggleTodo}
+                deleteTodo={deleteTodo}
+              />
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
